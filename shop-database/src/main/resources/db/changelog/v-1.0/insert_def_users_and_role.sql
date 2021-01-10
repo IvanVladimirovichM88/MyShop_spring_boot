af280a1a7be8f019286ae88c62934a1717f989b5
Ivan_M <opana1988@yandex.ru>
2021-01-11 01:32:31 +0300

    add sql with default users and roles

--- shop-database/src/main/resources/db/changelog/v-1.0/insert_def_users_and_role.sql
+++ shop-database/src/main/resources/db/changelog/v-1.0/insert_def_users_and_role.sql
@@ -1,6 +1,14 @@
-INSERT INTO `study_food_shop_db`.`user_tbl` (`name_fld`, `password_fld`, `username_fld`)
-VALUE ('Vasiliy', '$2y$12$A8SuVxN0Mu89CdKn3Bk1Yevuaayq8KEpXxh8Masvbw3Ke3ijNDokm', 'vasiliy'),
-        ('Alex', '$2y$12$A8SuVxN0Mu89CdKn3Bk1Yevuaayq8KEpXxh8Masvbw3Ke3ijNDokm', 'alex');
+INSERT INTO `user_tbl` (`username_fld`, `password_fld`)
+    VALUE   ('admin', '$2y$12$ysYXE5IpQo6SrwfLAvIA5.5l6VaLViRe0./G.k9WDCXN01D1LFP/2'),
+ 			('guest', '$2y$12$ysYXE5IpQo6SrwfLAvIA5.5l6VaLViRe0./G.k9WDCXN01D1LFP/2');
+GO
+INSERT INTO `role_tbl` (`name_fld`)
+    VALUE 	('ROLE_ADMIN'),
+ 		    ('ROLE_GUEST');
 GO
 
-insert into `study_food_shop_db`.`user_tbl` (`name_fld`, `password_fld`, `username_fld`)
+INSERT INTO `user_role_tbl`(`user_id`, `role_id`)
+    SELECT (SELECT `user_id` FROM `user_tbl` WHERE `username_fld` = 'admin'), (SELECT `role_id` FROM `role_tbl` WHERE `name_fld` = 'ROLE_ADMIN')
+UNION ALL
+    SELECT (SELECT `user_id` FROM `user_tbl` WHERE `username_fld` = 'guest'), (SELECT `role_id` FROM `role_tbl` WHERE `name_fld` = 'ROLE_GUEST');
+GO
